e79c51c647f2ab8b91736c233b6fa5453d855a3e
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-11-13 15:38:56 +0500

    Добавил список мероприятий
    #7000446

---
+++ frontend/src/components/EventList/EventList.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Grid, TextField, Typography } from '@mui/material';
+import { FilterListOutlined, SearchOutlined } from '@mui/icons-material';
+import EventCard from './EventCard/EventCard';
+import { StyledButton, StyledIconButton } from '../StyledControls/StyledControls';
+
+const EventList = () => {
+  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  return (
+    <>
+      <Typography variant="h1" component="h1" gutterBottom>
+        Мероприятия
+      </Typography>
+      <Grid component="article" container spacing={2.5} sx={{ mb: 5, alignItems: 'baseline' }}>
+        <Grid
+          item
+          lg={4}
+          md={6}
+          sm={6}
+          xs={6}
+          sx={{ display: 'flex', alignItems: 'flex-end' }}
+        >
+          <SearchOutlined fontSize="large" sx={{ mr: 1 }} />
+          <TextField
+            id="search"
+            label="Название мероприятия"
+            variant="standard"
+            sx={{ alignSelf: 'baseline', width: 220 }}
+          />
+          <StyledIconButton aria-label="filter" title="Фильтр" sx={{ ml: 'auto', mb: -1 }}>
+            <FilterListOutlined fontSize="large" />
+          </StyledIconButton>
+        </Grid>
+        <Grid
+          item
+          lg={8}
+          md={6}
+          sm={6}
+          xs={6}
+          sx={{
+            display: 'flex', gap: 2.5, alignItems: 'baseline',
+          }}
+        >
+          <StyledButton variant="outlined">
+            Екатеринбург
+          </StyledButton>
+          <StyledButton variant="outlined">
+            14 - 19 октября
+          </StyledButton>
+        </Grid>
+      </Grid>
+      <Grid component="article" container spacing={2.5}>
+        {arr.map(() => (
+          <EventCard />
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default EventList;
