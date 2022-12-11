4122ed15c1c8dc9e1bcf2ac13888031f12a0bb2f
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-12-12 00:33:49 +0500

    Добавил переход на меро

--- frontend/src/components/EventList/EventCard/EventCard.tsx
+++ frontend/src/components/EventList/EventCard/EventCard.tsx
@@ -1,40 +1,53 @@
-import React from 'react';
-import {
-  Avatar, Grid, Paper, Typography,
-} from '@mui/material';
-import styles from './EventCard.module.scss';
+import React from "react";
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
 
-const EventCard = () => (
-  <Grid item lg={4} md={6} sm={12} xs={12}>
-    <Paper elevation={2}>
-      <div className={styles.coverContainer}>
-        <img
-          className={styles.cover}
-          src="https://sun9-22.userapi.com/impg/Iys7Wp4VAftKbzly1TdVWFZM57Qxt3bbUrn97A/XfXfFlj5rb4.jpg?size=1367x906&quality=96&sign=51fd6e4fcd8c6768c7bb9ccb9049dca8&type=album"
-          alt="Обложка мероприятия"
-        />
-      </div>
-      <div className={styles.cardInfo}>
-        <Typography variant="h3" component="h3" sx={{ my: 1.5 }}>
-          Туса в Контуре
-        </Typography>
-        <div className={styles.company}>
-          <Avatar
-            className={styles.avatar}
-            src="https://sun3-11.userapi.com/impg/ky8437PNHYlH4yTRgJh2pPNSFKGbWjA8-dqaLw/jGVuPxkf_6s.jpg?size=650x925&quality=96&sign=fecf2fabbc55d170634f67222f9acf33&type=album"
+import styles from "./EventCard.module.scss";
+
+interface EventCardPropsType {
+  onClick: () => void;
+}
+
+const EventCard: React.FC<EventCardPropsType> = (props) => {
+  return (
+    <Grid item lg={4} md={6} sm={12} xs={12}>
+      <Paper className={styles.card} elevation={2} onClick={props.onClick}>
+        <div className={styles.coverContainer}>
+          <img
+            className={styles.cover}
+            src="https://sun9-22.userapi.com/impg/Iys7Wp4VAftKbzly1TdVWFZM57Qxt3bbUrn97A/XfXfFlj5rb4.jpg?size=1367x906&quality=96&sign=51fd6e4fcd8c6768c7bb9ccb9049dca8&type=album"
+            alt="Обложка мероприятия"
           />
-          <Typography variant="body1">Контур</Typography>
-          <Typography variant="body1">Серёга Чел</Typography>
         </div>
-        <Typography variant="body1" sx={{ textAlign: 'right', mt: 1.5, fontWeight: 300 }} gutterBottom>
-          Екатеринбург
-        </Typography>
-        <Typography variant="body1" sx={{ textAlign: 'right' }} gutterBottom>
-          31 февраля
-        </Typography>
-      </div>
-    </Paper>
-  </Grid>
-);
+        <div className={styles.cardInfo}>
+          <Typography variant="h3" component="h3" sx={{ my: 1.5 }}>
+            Туса в Контуре
+          </Typography>
+          <div className={styles.company}>
+            <Avatar
+              className={styles.avatar}
+              src="https://sun3-11.userapi.com/impg/ky8437PNHYlH4yTRgJh2pPNSFKGbWjA8-dqaLw/jGVuPxkf_6s.jpg?size=650x925&quality=96&sign=fecf2fabbc55d170634f67222f9acf33&type=album"
+            />
+            <Typography variant="body1">Контур</Typography>
+            <Typography variant="body1">Серёга Чел</Typography>
+          </div>
+          <Typography
+            variant="body1"
+            sx={{
+              textAlign: "right",
+              mt: 1.5,
+              fontWeight: 300,
+            }}
+            gutterBottom
+          >
+            Екатеринбург
+          </Typography>
+          <Typography variant="body1" sx={{ textAlign: "right" }} gutterBottom>
+            31 февраля
+          </Typography>
+        </div>
+      </Paper>
+    </Grid>
+  );
+};
 
 export default EventCard;
