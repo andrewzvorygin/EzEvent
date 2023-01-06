be08cf7546bb4d1e4c6043fad43ac5341fb61811
arthurshag <65594296+arthurshag@users.noreply.github.com>
2023-01-06 11:15:46 +0500

    Поправил редактор описания на четкий редактор описания

--- frontend/src/components/EventMaker/Description/Description.tsx
+++ frontend/src/components/EventMaker/Description/Description.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box, Typography } from "@mui/material";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import MuiMarkdown from "mui-markdown";
+import JoditEditor from "jodit-react";
 
-import Editor from "./Editor";
+import styles from "./Description.module.scss";
 
 interface DescriptionPropsType {
   ws: WebSocket;
@@ -12,37 +13,37 @@ interface DescriptionPropsType {
 }
 
 const Description: React.FC<DescriptionPropsType> = ({ ws, description }) => {
-  const [text, setText] = useState<string | undefined>("");
+  const editor = useRef(null);
+  const [content, setContent] = useState("Начните писать");
+  const config = {
+    readonly: false,
+    height: 400,
+  };
+  useEffect(() => {
+    if (description) {
+      setContent(description);
+    }
+  }, [description]);
+
+  const handleUpdate = (content: string) => {
+    setContent(content);
+  };
 
   function onChange(value: string) {
-    setText(value);
     ws.send(JSON.stringify({ description: value }));
   }
 
-  useEffect(() => {
-    setText(description);
-  }, [description]);
-
   return (
     <>
-      <Box maxHeight={500} overflow={"auto"}>
-        <MuiMarkdown
-          overrides={{
-            img: {
-              component: "img",
-              props: {
-                style: { width: "100%" },
-              },
-            },
-          }}
-        >
-          {text}
-        </MuiMarkdown>
-      </Box>
-      <Typography variant="h3" gutterBottom>
-        Описание мероприятия
-      </Typography>
-      <Editor value={text} onChange={onChange} />
+      <JoditEditor
+        className={styles.editor}
+        ref={editor}
+        value={content}
+        config={config}
+        onBlur={handleUpdate}
+        onChange={onChange}
+      />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
     </>
   );
 };
