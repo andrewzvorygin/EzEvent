cd4fa1df117805c945e9da8ef457536c3f96f235
Андрей <andrewzvorygin@yandex.ru>
2023-01-15 12:59:47 +0500

    отдаю json данные меро при подключении к ws

--- backend/src/application/api/event/websocket.py
+++ backend/src/application/api/event/websocket.py
@@ -3,14 +3,18 @@ from uuid import UUID
 
 from fastapi import WebSocket
 
+from .service import get_event
+
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[UUID, list] = defaultdict(list)
 
-    async def connect(self, websocket: WebSocket, event: UUID):
+    async def connect(self, websocket: WebSocket, event_uuid: UUID):
+        event = await get_event(event_uuid)
         await websocket.accept()
-        self.active_connections[event].append(websocket)
+        await websocket.send_text(event.json())
+        self.active_connections[event_uuid].append(websocket)
 
     async def disconnect(self, websocket: WebSocket, event: UUID):
         self.active_connections[event].remove(websocket)
